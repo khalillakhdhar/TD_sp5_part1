3804a633f1f7e0c99255807b5d58a0f0de553225
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 13:45:59 +0100

    Initial commit

---
+++ src/main/java/com/td1/TdSp5Part1Application.java
@@ -0,0 +1,13 @@
+package com.td1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdSp5Part1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdSp5Part1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/td1/TdSp5Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdSp5Part1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
