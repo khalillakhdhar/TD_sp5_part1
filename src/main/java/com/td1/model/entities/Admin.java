1634346ff459baafbe5145551e21b17cc6cbc8c5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 13:54:54 +0100

    Update Admin.java

--- src/main/java/com/td1/model/entities/Admin.java
+++ src/main/java/com/td1/model/entities/Admin.java
@@ -1,5 +1,91 @@
 package com.td1.model.entities;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+
 public class Admin {
 
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "nom")
+    private String nom;
+    @Basic(optional = false)
+    @Column(name = "prenom")
+    private String prenom;
+    @Basic(optional = false)
+    @Column(name = "mdp")
+    private String mdp;
+
+    public Admin() {
+    }
+
+    public Admin(Long id) {
+        this.id = id;
+    }
+
+    public Admin(Long id, String email, String nom, String prenom, String mdp) {
+        this.id = id;
+        this.email = email;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mdp = mdp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+   
 }
