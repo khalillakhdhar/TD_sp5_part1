507ad1973b7ae5aefea31148f23cb485b2d71ea1
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 14:11:59 +0100

    Update User.java

--- src/main/java/com/td1/model/entities/User.java
+++ src/main/java/com/td1/model/entities/User.java
@@ -16,7 +16,6 @@ import javax.persistence.Id;
 
 public class User {
 
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
@@ -114,29 +113,6 @@ public class User {
         this.grade = grade;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof User)) {
-            return false;
-        }
-        User other = (User) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "javaapplication6.User[ id=" + id + " ]";
-    }
     
 }
