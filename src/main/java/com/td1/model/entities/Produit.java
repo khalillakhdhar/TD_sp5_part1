67ceadebc7413591c63c26d9523bdbcdaa51529e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 14:11:50 +0100

    Update Produit.java

--- src/main/java/com/td1/model/entities/Produit.java
+++ src/main/java/com/td1/model/entities/Produit.java
@@ -20,7 +20,6 @@ import javax.persistence.Table;
 
 public class Produit  {
 
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
@@ -119,29 +118,6 @@ public class Produit  {
         this.categorie = categorie;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Produit)) {
-            return false;
-        }
-        Produit other = (Produit) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "javaapplication6.Produit[ id=" + id + " ]";
-    }
+ 
 }
     
