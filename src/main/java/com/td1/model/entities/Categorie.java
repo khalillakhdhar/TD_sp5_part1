025289535a093375ee24097653550e20c4485ea3
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 13:56:19 +0100

    Update Categorie.java

--- src/main/java/com/td1/model/entities/Categorie.java
+++ src/main/java/com/td1/model/entities/Categorie.java
@@ -1,5 +1,58 @@
 package com.td1.model.entities;
 
-public class Categorie {
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author TPC
+ */
+@Entity
+@Table(name = "categorie")
+
+public class Categorie implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "titre")
+    private String titre;
+
+    public Categorie() {
+    }
+
+    public Categorie(Integer id) {
+        this.id = id;
+    }
+
+    public Categorie(Integer id, String titre) {
+        this.id = id;
+        this.titre = titre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
 
 }
