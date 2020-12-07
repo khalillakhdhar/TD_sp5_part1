6da35446b5ec334343d24d78b15411b865c55146
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-07 13:59:27 +0100

    Update Reclamation.java

--- src/main/java/com/td1/model/entities/Reclamation.java
+++ src/main/java/com/td1/model/entities/Reclamation.java
@@ -1,5 +1,109 @@
 package com.td1.model.entities;
 
-public class Reclamation {
 
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author TPC
+ */
+@Entity
+
+public class Reclamation  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "titre")
+    private String titre;
+    @Basic(optional = false)
+    @Column(name = "texte")
+    private String texte;
+    @Basic(optional = false)
+    @Column(name = "date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    public Reclamation() {
+    }
+
+    public Reclamation(Integer id) {
+        this.id = id;
+    }
+
+    public Reclamation(Integer id, String titre, String texte, Date date) {
+        this.id = id;
+        this.titre = titre;
+        this.texte = texte;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Reclamation)) {
+            return false;
+        }
+        Reclamation other = (Reclamation) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "javaapplication6.Reclamation[ id=" + id + " ]";
+    }
+    
 }
